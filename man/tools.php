558f571fc7cdf405b96ac1152c54861227f6dc08
sernuzh <sernuzh@gmail.com>
2026-02-05 11:52:46 +0200

    2026-02-05

--- man/tools.php
+++ man/tools.php
@@ -8,4 +8,5 @@ https://www.figma.com
 https://www.canva.com
 https://www.vistaprint.com/logomaker
 https://www.vidnoz.com/text-to-video-ai.html  - там є кнопка create an add
+https://use.ai/chat/4210b0a2-5b7c-4606-8ab4-0365436e6f8a
 
