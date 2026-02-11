1399bec3529d89a3587cd785d8b7a163dc2af6b4
sernuzh <sernuzh@gmail.com>
2026-02-11 16:32:42 +0200

    2026-02-11

--- man/tools.php
+++ man/tools.php
@@ -6,9 +6,11 @@ https://redesignr.ai/dashboard
 
 https://www.figma.com
 https://www.canva.com
+https://picsart.com/colors/color-picker/
 https://www.vistaprint.com/logomaker
 https://www.vidnoz.com/text-to-video-ai.html  - там є кнопка create an add
 https://use.ai/chat/4210b0a2-5b7c-4606-8ab4-0365436e6f8a
 https://firefly.adobe.com/generate/image
 https://app.gendo.ai/project/421863c9-5a58-4552-82e9-7a0af6700116
+
 
